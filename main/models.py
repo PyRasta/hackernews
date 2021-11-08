655215fb7c0d66299062867e5d6242d001c5f820
PyRasta <daniilrastorguev01@gmail.com>
2021-11-08 22:41:48 +0500

    Аккаунты могут стать модерами

--- main/models.py
+++ main/models.py
@@ -1,35 +1,39 @@
 from django.db import models
-from django.db.models.fields import DateField
 
 
 class Article(models.Model):
     title = models.CharField("Название", max_length=100)
     url = models.URLField('Адрес', max_length=150)
+    url_good = models.CharField("Отредактированный адрес", max_length=150, default='SOME STRING')
     author = models.CharField("Автор", max_length=30)
     date = models.DateField(auto_now_add=True)
-    time = models.TimeField(auto_now_add=True) 
+    time = models.TimeField(auto_now_add=True)
 
     def __str__(self):
-        return self.title    
+        return self.title
 
     class Meta:
         verbose_name = "Статья"
-        verbose_name_plural = "Статьи"    
-        
+        verbose_name_plural = "Статьи"
+
+
 class CreateAccount(models.Model):
     name = models.CharField("Пользователь", max_length=30)
-    password = models.CharField("Пароль", max_length=50)       
+    password = models.CharField("Пароль", max_length=50)
+    moder = models.BooleanField("Модератор", default=False)
 
     def __str__(self):
         return self.name
 
     class Meta:
         verbose_name = "Пользователь"
-        verbose_name_plural = "Пользователи"    
+        verbose_name_plural = "Пользователи"
+
 
 class FindAccount(models.Model):
-    username = models.CharField("Авторизированный Пользователь", max_length=30) 
-    password = models.CharField("Пароль", max_length=50)  
+    username = models.CharField("Авторизированный Пользователь", max_length=30)
+    password = models.CharField("Пароль", max_length=50)
+    moder = models.BooleanField("Модератор", default=False)
     ip = models.CharField("IP", max_length=50)
 
     def __str__(self):
@@ -37,7 +41,8 @@ class FindAccount(models.Model):
 
     class Meta:
         verbose_name = "Авторизированный Пользователь"
-        verbose_name_plural = " Авторизированные Пользователи"     
+        verbose_name_plural = " Авторизированные Пользователи"
+
 
 class CheckPost(models.Model):
     title = models.CharField("Название", max_length=100)
@@ -49,4 +54,4 @@ class CheckPost(models.Model):
 
     class Meta:
         verbose_name = "Статья для проверки"
-        verbose_name_plural = "Статьи для проверки"    
+        verbose_name_plural = "Статьи для проверки"
--- main/views.py
+++ main/views.py
@@ -10,14 +10,17 @@ def get_client_ip(request):
         ip = x_forwarded_for.split(',')[0]
     else:
         ip = request.META.get('REMOTE_ADDR')
-    return ip    
+    return ip
+
 
 def get_user_id():
     online_bd = FindAccount.objects.order_by("-id")[:1]
     for el in online_bd:
-        return el.id   
+        return el.id
+
 
 def check_online(request):
+    moder = bool
     flag = bool
     username = ""
     online = FindAccount.objects.all()
@@ -26,14 +29,22 @@ def check_online(request):
         if user_ip == el.ip:
             flag = True
             username = el.username
-            break        
+            break
         else:
             flag = False
+    for el in online:
+        if el.moder:
+            moder = True
+            break
+        else:
+            moder = False
     check = {
         "username": username,
-        "flag": flag
-    }        
+        "flag": flag,
+        "moder": moder
+    }
     return check
+
 
 def delete_online(request):
     username = ''
@@ -41,20 +52,25 @@ def delete_online(request):
     online = FindAccount.objects.all()
     for el in online:
         if user_ip == el.ip:
-            username = el.username    
+            username = el.username
             break
         else:
             return redirect('home')
-    user_delete = FindAccount.objects.get(username = username)  
-    user_delete.delete()  
+    user_delete = FindAccount.objects.get(username=username)
+    user_delete.delete()
     return redirect('home')
+
 
 def update_bd(request):
     user_ip = get_client_ip(request)
     user_id = get_user_id()
-    user = FindAccount.objects.get(id = user_id)
+    user = FindAccount.objects.get(id=user_id)
+    username = user.username
+    accounts = CreateAccount.objects.get(name=username)
     user.ip = user_ip
+    user.moder = accounts.moder
     user.save()
+
 
 def logreg(request):
     find_error = ''
@@ -69,10 +85,9 @@ def logreg(request):
             for el in base:
                 if el.name == name:
                     create_error = "Этот пользователь уже есть!!"
-                    break 
+                    break
             if create_error == "":
-                create_form.save()  
-
+                create_form.save()
 
         if request.POST.get('username'):
             username = request.POST['username']
@@ -89,10 +104,10 @@ def logreg(request):
                     update_bd(request)
                     return redirect("home")
                 else:
-                    find_error = "Неверный пароль" 
+                    find_error = "Неверный пароль"
 
             if find_error == '':
-                find_error = "Неправильно введено имя пользователя"              
+                find_error = "Неправильно введено имя пользователя"
 
     create_form = CreateForm()
     find_form = FindForm()
@@ -102,32 +117,36 @@ def logreg(request):
         'create_error': create_error,
         'find_error': find_error
     }
-    return render(request, 'main/reg_log.html', contex)   
+    return render(request, 'main/reg_log.html', contex)
+
 
 def date_later():
     datetime = []
-    articls = Article.objects.order_by("id")[:30]
-    for el in articls:
+    articles = Article.objects.order_by("id")[:30]
+    for el in articles:
         pass
 
-def url_filter():
-    url_filt = []
-    articls = Article.objects.order_by("id")
-    for el in articls:
-         url_filt.append(el.url.replace("https://",""))
-    return url_filt     
+
+def number_articles():
+    articles = CheckPost.objects.all()
+    number_articles = 0
+    for el in articles:
+        number_articles += 1
+    return number_articles
+
 
 def index(request):
     check = check_online(request)
-    articls = Article.objects.order_by("id")  
-    url_good = url_filter()
+    articles = Article.objects.order_by("id")
+    number = number_articles()
 
     contex = {
-        'articls': articls,
+        'articles': articles,
         'check': check,
-        "url_good": url_good
+        "number": number
     }
-    return render(request, 'main/index.html', contex)     
+    return render(request, 'main/index.html', contex)
+
 
 def new(request):
     check = check_online(request)
@@ -136,12 +155,14 @@ def new(request):
     }
     return render(request, 'main/new.html', contex)
 
+
 def past(request):
     check = check_online(request)
     contex = {
         'check': check,
     }
     return render(request, 'main/past.html', contex)
+
 
 def comments(request):
     check = check_online(request)
@@ -150,12 +171,14 @@ def comments(request):
     }
     return render(request, 'main/comments.html', contex)
 
+
 def ask(request):
     check = check_online(request)
     contex = {
         'check': check,
     }
     return render(request, 'main/ask.html', contex)
+
 
 def show(request):
     check = check_online(request)
@@ -164,6 +187,7 @@ def show(request):
     }
     return render(request, 'main/show.html', contex)
 
+
 def jobs(request):
     check = check_online(request)
     contex = {
@@ -171,10 +195,12 @@ def jobs(request):
     }
     return render(request, 'main/jobs.html', contex)
 
+
 def get_post_id():
     bd_checkpost = CheckPost.objects.order_by("-id")[:1]
     for el in bd_checkpost:
-        return el.id     
+        return el.id
+
 
 def get_author(request):
     online = FindAccount.objects.all()
@@ -182,14 +208,16 @@ def get_author(request):
     for el in online:
         if user_ip == el.ip:
             username = el.username
-            break        
+            break
     return username
+
 
 def update_post(request):
     id = get_post_id()
-    post = CheckPost.objects.get(id = id)
+    post = CheckPost.objects.get(id=id)
     post.author = get_author(request)
     post.save()
+
 
 def submit(request):
     error = ""
@@ -200,7 +228,7 @@ def submit(request):
             update_post(request)
             return redirect("home")
         else:
-            error = "Неверно введен url адрес"    
+            error = "Неверно введен url адрес"
 
     check = check_online(request)
     article_form = ArticleForm()
@@ -211,11 +239,14 @@ def submit(request):
     }
     return render(request, 'main/submit.html', contex)
 
+
 def welcome(request):
-    return render(request, "main/welcome.html",)
+    return render(request, "main/welcome.html", )
+
 
 def guidelines(request):
-    return render(request, "main/guidelines.html",)
+    return render(request, "main/guidelines.html", )
+
 
 def threads(request):
     check = check_online(request)
